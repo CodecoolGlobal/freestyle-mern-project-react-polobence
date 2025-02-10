a4e0972de45f30bc78788bf8e7cff3d3a6fc2351
polobence <polovicsbence@gmail.com>
2025-02-10 10:33:56 +0100

    set up default server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
 
-const app=express();
-const PORT=3005;
+const app = express();
+const PORT = 3005;
 
+mongoose
+  .connect("mongodb+srv://polobence:Pds56ghj@cluster0.23pel.mongodb.net/")
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});

c8cf768224ffbf675e717d511b6f8f61c6ed514e
polobence <polovicsbence@gmail.com>
2025-02-13 13:31:30 +0100

    add link for development

--- client/src/Pages/Menu.jsx
+++ client/src/Pages/Menu.jsx
@@ -9,17 +9,19 @@ const Menu = () => {
     <>
       <div className="menu">
         <nav>
-          <h1>Gamer's Wish Vault </h1>
+          <Link className="logo" to="/">
+            <h1>Gamer's Wish Vault </h1>
+          </Link>
           <div className="nav-buttons">
-          <Link to={`/u/games/${userId}`}>
-            {userId ? <button>Games</button> : <button disabled>Games</button>}
-          </Link>
-          <Link to={`/u/wishlist/${userId}`}>
-            {userId ? <button>My Wish List</button> : <button disabled>My Wish List</button>}
-          </Link>
-          <Link to={`/u/account/${userId}`}>
-            {userId ? <button>My Account</button> : <button disabled>My Account</button>}
-          </Link>
+            <Link to={`/u/games/${userId}`}>
+              {userId ? <button>Games</button> : <button disabled>Games</button>}
+            </Link>
+            <Link to={`/u/wishlist/${userId}`}>
+              {userId ? <button>My Wish List</button> : <button disabled>My Wish List</button>}
+            </Link>
+            <Link to={`/u/account/${userId}`}>
+              {userId ? <button>My Account</button> : <button disabled>My Account</button>}
+            </Link>
           </div>
         </nav>
         <div className="main">

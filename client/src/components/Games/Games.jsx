be3225ea72ba97bfafb7c8ed0aa5ce6c406f063a
Gecse David <dadi.gecse@gmail.com>
2025-02-27 14:09:37 +0100

    quick fixes for presentation

--- client/src/components/Games/Games.jsx
+++ client/src/components/Games/Games.jsx
@@ -6,18 +6,76 @@ import NavButtons from "../NavButtons/NavButtons";
 import PageSizeControl from "../PageSizeControl/PageSizeControl";
 import FilterControls from "../FilterControls/FilterControls";
 
+const fetchGenres = async () => {
+  try {
+    const res = await fetch("/api/genres");
+    return await res.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const fetchStores = async () => {
+  try {
+    const res = await fetch("/api/stores");
+    return await res.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const fetchPlatforms = async () => {
+  try {
+    const res = await fetch("/api/platforms");
+    return await res.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 function Games() {
   const [games, setGames] = useState(null);
   const [page, setPage] = useState(1);
   const [pageSize, setPageSize] = useState(10);
   const [loading, setLoading] = useState(true);
+  const [search, setSearch] = useState(null);
+  const [filters, setFilters] = useState({
+    genre: null,
+    store: null,
+    platform: null
+  })
+  const [genres, setGenres] = useState(null);
+  const [stores, setStores] = useState(null);
+  const [platforms, setPlatforms] = useState(null);
+  const [filterLoading, setFilterLoading] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
+      setGenres(await fetchGenres());
+      setStores(await fetchStores());
+      setPlatforms(await fetchPlatforms());
+    }
+
+    try {
+      fetchData();
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setFilterLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    async function fetchData() {
+      let filterUrl = `/api/games/${page}/${pageSize}?`;
+        if (filters.genre) filterUrl += `&genre=${filters.genre.toLowerCase()}`;
+        if (filters.platform) filterUrl += `&platform=${filters.platform.toLowerCase()}`;
+        if (filters.store) filterUrl += `&store=${filters.store.toLowerCase()}`;
       try {
-        const data = await fetch(`/api/games/${page}/${pageSize}`).then((res) =>
-          res.json()
-        );
+        const url = search
+          ? `/api/search/${search}/${page}/${pageSize}`
+          : filterUrl;
+        const data = await fetch(url).then((res) => res.json());
         setGames(data);
       } catch (error) {
         console.error(error);
@@ -26,19 +84,22 @@ function Games() {
       }
     }
     fetchData();
-  }, [page, pageSize]);
+  }, [page, pageSize, search, filters]);
 
-  async function handleSearch(search) {
-    try {
-      const res = await fetch(`/api/search/${search}/${page}/${pageSize}`);
-      const games = await res.json();
-      setGames(games);
-    } catch (err) {
-      console.error(err);
-    }
+  
+
+  function handleSearch(search) {
+      setSearch(search);
+      setPage(1);
+  }
+
+  function handleFilters(filters) {
+    setFilters(filters);
+    setPage(1);
   }
 
   function handleNextPage() {
+    if(page < Math.ceil(games.count / pageSize))
     setPage((prevPage) => prevPage + 1);
   }
 
@@ -49,7 +110,7 @@ function Games() {
     return;
   }
 
-  if (loading) return <Loading />;
+  if (loading || filterLoading) return <Loading />;
   if (!games) return <p>Error fetching games.</p>;
 
   return (
@@ -60,9 +121,10 @@ function Games() {
         onStepNext={handleNextPage}
         onStepPrev={handlePrevPage}
         page={page}
+        totalPage={Math.ceil(games.count / pageSize)}
       />
 
-      <FilterControls handleSearch={handleSearch} />
+      <FilterControls handleSearch={handleSearch} handleFilters={handleFilters} genres={genres} stores={stores} platforms={platforms}/>
 
       <PageSizeControl pageSize={pageSize} setPageSize={setPageSize} />
 
@@ -76,6 +138,7 @@ function Games() {
         onStepNext={handleNextPage}
         onStepPrev={handlePrevPage}
         page={page}
+        totalPage={Math.ceil(games.count / pageSize)}
       />
     </div>
   );
--- server/utilities/fetchGames.js
+++ server/utilities/fetchGames.js
@@ -3,9 +3,9 @@ import { API_KEY } from "../config.js";
 
 export async function fetchGames( page, pageSize = 10, filters = null,) {
   let url = `${API_URL}?key=${API_KEY}&page=${page}&page_size=${pageSize}`;
-  if (filters.genre) url + `&genre=${filters.genre}`;
-  if (filters.platform) url + `&platform=${filters.platform}`;
-  if (filters.store) url + `&store=${filters.store}`;
+  if (filters.genre) url += `&genres=${filters.genre.replaceAll(" ", "%20")}`;
+  if (filters.platform) url += `&platforms=${filters.platform}`;
+  if (filters.store) url += `&stores=${filters.store}`;
   try {
     const response = await fetch(url);
     if (!response.ok) {
